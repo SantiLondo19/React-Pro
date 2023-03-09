f5952045f3952a8dbb42aec0c4e3fe6a06067a06
santiago echavarria  londoño <gamersanti19@gmail.com>
2023-03-09 12:42:36 -0500

    extended styles

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -8,10 +8,15 @@ import {
 import "../styles/custom-styles.css";
 export const ShoppingPage = () => {
   const product = {
+    id: 1,
     title: "Coffee Mug - card",
     img: "./coffee-mug.png",
   };
-
+  const product2 = {
+    id: 2,
+    title: "Coffee Mug - Meme",
+    img: "./coffee-mug2.png",
+  };
   return (
     <div>
       <h1>Shopping Store</h1>
@@ -28,15 +33,10 @@ export const ShoppingPage = () => {
           <ProductCard.Title className="text-bold" />
           <ProductCard.Button className="custom-buttons" />
         </ProductCard>
-        <ProductCard product={product} className="bg-dark text-white">
+        <ProductCard product={product2} className="bg-dark text-white">
           <ProductImage className="img" />
           <ProductTitle className="text-bold" />
           <ProductButton className="custom-buttons" />
-        </ProductCard>
-        <ProductCard product={product} style={{ backgroundColor: "red"}}>
-          <ProductImage />
-          <ProductTitle />
-          <ProductButton />
         </ProductCard>
       </div>
     </div>
